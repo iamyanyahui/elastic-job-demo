1877c81dd67f04990a890fdc7c80bf628743c38c
yanyahui <yanyahui@kuaishou.com>
2021-12-13 15:52:01 +0800

    add

--- daily-test/src/main/java/com/yahui/GaussianDistribution.java
+++ daily-test/src/main/java/com/yahui/GaussianDistribution.java
@@ -1,5 +1,21 @@
-package com.yahui;/**
+package com.yahui;
+
+/**
  * @author yanyahui <yanyahui@kuaishou.com>
  * Created on 2021-09-13
- */public class GaussianDistribution {
+ */
+public class GaussianDistribution {
+    private static final long seed = 1234567L;
+    private static final double averageSqrt = Math.sqrt(100);
+    private static final int sig = 1;
+
+    public static void main(String[] args) {
+//        Random random = new Random(seed);
+//        final List<Double> collect = IntStream.range(0, 1000)
+//                .mapToObj(i -> random.nextGaussian() * averageSqrt + sig)
+//                .collect(Collectors.toList());
+//        System.out.println(collect);
+
+        System.out.println(100 % -7);
+    }
 }
--- daily-test/src/main/java/com/yahui/Main.java
+++ daily-test/src/main/java/com/yahui/Main.java
@@ -1,5 +1,36 @@
-package com.yahui;/**
+package com.yahui;
+
+import java.math.BigDecimal;
+
+/**
  * @author yanyahui <yanyahui@kuaishou.com>
  * Created on 2021-08-31
- */public class Main {
+ */
+public class Main {
+    private static final int NUM_1W = 10000;
+    public static String getDisplayText(long value) {
+        if (value < NUM_1W) {
+            return String.valueOf(value);
+        }
+        BigDecimal bd = BigDecimal.valueOf(((double) value) / NUM_1W);
+        String p = String.valueOf(bd.setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue());
+        return p + "万";
+    }
+
+    // CHECKSTYLE:OFF
+    public static void main(String[] args) {
+//        System.out.println(getDisplayText(1000));
+//        System.out.println(getDisplayText(10712));
+//        System.out.println(getDisplayText(10515));
+//        System.out.println(getDisplayText(10415));
+//        System.out.println(getDisplayText(10117));
+
+//        IntStream.range(0, 100).forEach(i -> {
+//            System.out.println(((double) (Long.MAX_VALUE - System.currentTimeMillis())) / Long.MAX_VALUE);
+//        });
+
+
+
+    }
+    // CHECKSTYLE:ON
 }
